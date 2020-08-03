9faff31fa4888cc7cc6713ae5c0e82f7bd790c2c
Armin Karimi Birgani <armin.rkb@gmail.com>
2020-08-03 19:21:06 +0200

    [Add] Player movement. [Add] Walking and idle animation for player. [Change] Moved location of gitignore and readme.

---
+++ TAPESBOUND/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Stats:")]
+    [SerializeField] private float movementSpeed = 0;
+    private bool isWalking;
+    private Vector2 lastDirection;
+    private Vector2 movementDirection;
+
+    [Header("References:")]
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private Rigidbody2D rb = null;
+
+    void Update()
+    {
+        FlipAnimation();
+        Animate();
+    }
+
+    void FixedUpdate()
+    {
+        GetInput();
+        Move();
+    }
+
+    private void GetInput()
+    {
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementDirection.Normalize();
+        isWalking = false;
+
+        if (movementDirection.x != 0 || movementDirection.y != 0)
+        {
+            isWalking = true;
+            lastDirection = movementDirection;
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed;
+    }
+
+    private void FlipAnimation()
+    {
+        if (isWalking)
+        {
+            if (movementDirection.x > 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            if (lastDirection.x > 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+    }
+
+    private void Animate()
+    {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+        animator.SetFloat("LastHorizontal", lastDirection.x);
+        animator.SetFloat("LastVertical", lastDirection.y);
+        animator.SetBool("IsWalking", isWalking);
+    }
+}
