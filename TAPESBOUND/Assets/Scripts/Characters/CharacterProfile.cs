9bb17cde3db53cad98d8ba080ac2500f9e02bad6
Armin Karimi Birgani <armin.rkb@gmail.com>
2020-09-01 16:00:15 +0200

    [Add] Scene transition prototype.

--- TAPESBOUND/Assets/Scripts/Characters/CharacterProfile.cs
+++ TAPESBOUND/Assets/Scripts/Characters/CharacterProfile.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu (fileName = "Character", menuName = "Character/New Character", order = 1)]
 public class CharacterProfile : ScriptableObject
@@ -9,7 +6,7 @@ public class CharacterProfile : ScriptableObject
     [Header("Name of character")]
     public string characterName;
     [Header("Animator controller")]
-    public AnimatorController animatorController;
+    public RuntimeAnimatorController animatorController;
     [Header("Current Level")]
     public int level;
     public int xp;
